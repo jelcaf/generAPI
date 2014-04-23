acfeb85adb5721989fe772f53a63632165019560
Paco Martin Fernandez <erespia2@gmail.com>
2014-04-23 13:38:43 +0100

    FrontEnd with file upload without sending to the server yet.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,5 +2,39 @@
   "use strict";
 
   var app = angular.module("generAPI", ['ui.bootstrap']);
-
+  app.directive('fileSelect', function(){
+    return {
+        link: function($scope,el){
+      		el.bind("change", function(e){
+        		$scope.file = (e.srcElement || e.target).files[0];
+        		$scope.getFile();
+      		})   
+    	}
+    };
+  });
 }());
+
+$('input[type="file"]').inputfile({
+    uploadText: '<span class="glyphicon glyphicon-upload"></span> Select a CSV File',
+    removeText: '<span class="glyphicon glyphicon-trash"></span>',
+    restoreText: '<span class="glyphicon glyphicon-remove"></span>',
+
+    uploadButtonClass: 'btn btn-primary',
+    removeButtonClass: 'btn btn-default'
+});
+
+var UploadController = function ($scope, fileReader) {
+     console.log(fileReader)
+     $scope.getFile = function () {
+        $scope.progress = 0;
+        fileReader.readAsText($scope.file, $scope)
+                  .then(function(result) {
+                     processCSV(result);
+                  });
+     };
+};
+
+var processCSV = function(csv){
+	// TODO: Send CSV to Server and get respond with a possible API structure.
+	console.log(csv);
+}
